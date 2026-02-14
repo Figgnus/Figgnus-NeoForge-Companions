0311fbeadb35e9f6aa9d9a89f90e9117377af64b
Figgnus <dav33mail@gmail.com>
2026-02-14 20:43:14 +0100

    Initial commit

--- src/main/java/me/figgnus/floatingcompanions/FloatingCompanionsMod.java
+++ src/main/java/me/figgnus/floatingcompanions/FloatingCompanionsMod.java
@@ -1,5 +1,6 @@
 package me.figgnus.floatingcompanions;
 
+import me.figgnus.floatingcompanions.block.ModBlocks;
 import me.figgnus.floatingcompanions.client.ClientBootstrap;
 import me.figgnus.floatingcompanions.command.PetCommand;
 import me.figgnus.floatingcompanions.network.PetNetwork;
@@ -16,6 +17,7 @@ public final class FloatingCompanionsMod {
     public static final String MOD_ID = "floatingcompanions";
 
     public FloatingCompanionsMod(IEventBus modEventBus, ModContainer modContainer) {
+        ModBlocks.register(modEventBus);
         modEventBus.addListener(PetNetwork::registerPayloadHandlers);
 
         NeoForge.EVENT_BUS.addListener(PetCommand::onRegisterCommands);
